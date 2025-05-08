289b3af2af857244487ca355f74fcfe52e49c9f2
Clayton Bentes <claytonbentes@live.com>
2025-05-07 21:01:13 -0300

    feat: Implementando estilos usados no projeto

---
+++ src/styles/colors.ts
@@ -0,0 +1,22 @@
+export const colors = {
+    gray: {
+      100: "#17222B",
+      200: "#293C4C",
+      300: "#4D708F",
+      400: "#A2B9CD",
+      500: "#CAD7E2",
+      600: "#D7E1EA",
+      700: "#E8EEF3",
+      800: "#F8FAFB",
+    },
+  
+    blue: {
+      base: "#334FDC",
+      light: "#6E83EC"
+    },
+  
+    red: {
+      light: "#EB3C4D",
+      base: "#C02636",
+    },
+  }
---
+++ src/styles/font-family.ts
@@ -0,0 +1,5 @@
+export const fontFamily = {
+    extrabold: "NunitoSans_800ExtraBold",
+    regular: "NunitoSans_400Regular",
+    semiBold: "NunitoSans_600SemiBold",
+  }
